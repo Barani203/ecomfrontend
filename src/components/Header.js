85283b4891a6de1872903eafa3eee32135adf9a2
Barani203 <92353632+Barani203@users.noreply.github.com>
2022-03-22 03:08:05 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
     return (
         <header>
             <Navbar 
-                    bg="muted" 
+                    bg="success" 
                     variant="dark"
                     expand="lg"
                     collapseOnSelect>
